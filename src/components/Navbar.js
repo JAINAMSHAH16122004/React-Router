c574d09915e2a77bd9483386d00fa9b38a80d334
jainam <jainamms1612@gmail.com>
2024-07-19 17:17:40 +0530

    changed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import {createBrowserRouter,Outlet,RouterProvider} from 'react-router-dom'
+import {Outlet} from 'react-router-dom'
 
 import {Link} from 'react-router-dom';
 
